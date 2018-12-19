3950999d00dd6ef5864f48d24779d52a2fc82f32
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-19 00:37:35 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,17 @@
 def reformat_languages(languages)
   # your code here
   
-  
+  new_hash = {}
+  # nested_hash1 = {}
+  # nested_hash2 = {}
+
+  hehe.each do |difference, language_names|
+    language_names.each do |language, type|
+      new_hash[language] = type
+      new_hash[language][:difference] = []
+      new_hash[language][:difference] << difference
+    end
+  end
+
+  new_hash
 end
